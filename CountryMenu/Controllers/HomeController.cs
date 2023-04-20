6d65234ab5a51c63dba5f865c0b90ccc351e8b37
Syed Abdul Basit <79485785+sbasit57@users.noreply.github.com>
2023-04-20 12:16:35 +0500

    Add project files.

---
+++ CountryMenu/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CountryMenu.Controllers
+{
+    public class HomeController : Controller
+    {
+        CountryMenuEntities db = new CountryMenuEntities();
+        public ActionResult Index()
+        {
+            List<Country> CountryList = db.Countries.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "CountryId", "CountryName");
+            return View();
+        }
+
+        public JsonResult GetStateList(int CountryId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<State> Statelist = db.States.Where(x => x.CountryId == CountryId).ToList();
+            return Json(Statelist, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CountryMenu/Models/CountryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CountryMenu.Models
+{
+    public class CountryViewModel
+    {
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+    }
+}
